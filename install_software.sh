e96a0d7d17d2495e8e17ea91266966e8a7ee73fd
Salvatore Rappoccio <rappoccio@gmail.com>
2018-07-20 16:36:47 +0200

    Adding py2 and py3

--- install_software.sh
+++ install_software.sh
@@ -1,13 +1,14 @@
 # Ubuntu+tools, python, pip, python modules
 apt-get update
-apt-get install -y wget g++ libtool rsync make python3 python-dev python-numpy python-pip
+apt-get install -y wget g++ libtool rsync make python2-dev python2-numpy python2-pip python3-dev python3-numpy python3-pip
 rm -rf /var/lib/apt/lists/*
 pip install --no-cache-dir matplotlib scipy numpy scikit-learn keras tensorflow jupyter metakernel zmq notebook==5.* plaidml-keras plaidbench energyflow
+pip3 install --no-cache-dir matplotlib scipy numpy scikit-learn keras tensorflow jupyter metakernel zmq notebook==5.* plaidml-keras plaidbench energyflow
 # fastjet
 wget http://fastjet.fr/repo/fastjet-3.3.1.tar.gz \
     && tar xzf fastjet-3.3.1.tar.gz && rm fastjet-3.3.1.tar.gz \
     && cd fastjet-3.3.1 \
-    && ./configure --prefix=/app --enable-python && make && make install \
+    && ./configure --prefix=/app --enable-pyext && make && make install \
     && cd .. 
 # fastjet-contrib
 wget http://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.036.tar.gz \
